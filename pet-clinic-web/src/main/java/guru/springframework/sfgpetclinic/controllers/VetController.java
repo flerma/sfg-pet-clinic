5baddcf78196fa19a1965f12c07fadbdf2fedebd
Fernando Marchesano Lerma <fernando.lerma@gmail.com>
2022-01-21 18:43:25 -0300

    Create Vet Index Page Controller. closes #7

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
@@ -0,0 +1,17 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author flerma
+ */
+@Controller
+public class VetController {
+
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    public String listVets() {
+
+        return "vets/index";
+    }
+}
